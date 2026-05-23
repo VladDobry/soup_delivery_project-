621655fe3470a67be683596125a0b10873c90158
vladdobrynin <vladdobry666@gmail.com>
2026-05-23 21:50:57 +0300

    urls

--- config/settings.py
+++ config/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ALLOWED_HOSTS = [
+    '.onrender.com'
     "127.0.0.1",
     "localhost",
     ".ngrok-free.app",
